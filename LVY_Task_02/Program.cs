e29e2f4bf8c6ccbccddff0659ac1f7ef5d14094f
VladLugovskiy <74325987+VladLugovskiy@users.noreply.github.com>
2020-11-14 16:34:39 +0300

    Add files via upload

---
+++ LVY_Task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVY_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any word: ");
+            string word = " ";
+            do
+            {
+                word = Console.ReadLine();
+            }
+            while (word != ("exit"));
+            Console.WriteLine("The program has finished working. ");
+        }
+    }
+}
